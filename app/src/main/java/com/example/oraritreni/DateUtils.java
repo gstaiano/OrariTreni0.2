743baf3ba14598e7bdff43257f2654a630fc67c9
gstaiano <e444jack@gmail.com>
2019-08-02 00:21:57 +0200

    RFI-5

--- app/src/main/java/com/example/oraritreni/DateUtils.java
+++ app/src/main/java/com/example/oraritreni/DateUtils.java
@@ -7,13 +7,27 @@ import java.util.Date;
 
 public class DateUtils {
     //Classe di utilità che si occupa di convertire il formato data string della response in un formato adatto alla UI.
-    public static String formatDate(String dateString){
+    public static String formatDateFromResponse(String dateString){
         String dateFormatted="";
         try {
             String pattern = "yyyy-MM-dd'T'HH:mm:ss";
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
             Date date = simpleDateFormat.parse(dateString);
             DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+            dateFormatted=dateFormat.format(date);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return dateFormatted;
+    }
+
+    public static String formatDateForRequest(String dateString){
+        String dateFormatted="";
+        try {
+            String pattern = "d-M-yyyy";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            Date date = simpleDateFormat.parse(dateString);
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
             dateFormatted=dateFormat.format(date);
         }catch(ParseException e){
             e.printStackTrace();
